148489e13b1f57cdb0e151967b25fbab3460e8c7
Neil Benjamin Roble <neilbenjaminroble@gmail.com>
2026-06-17 17:05:01 +0800

    added buttons and replaced q-input with q-select

--- src/pages/ItemsAdjustmentForm.vue
+++ src/pages/ItemsAdjustmentForm.vue
@@ -5,8 +5,7 @@
         <q-card-section>
           <div class="text-h6 text-green">Items Adjustment Form</div>
           <div class="text-caption text-grey">
-            This is the items adjustment form. You can use this form to adjust the items quantity in
-            the inventory.
+            You can use this form to adjust the items quantity in the inventory.
           </div>
         </q-card-section>
         <q-separator />
@@ -21,47 +20,85 @@
 
           <!-- Row 2: 4 equal columns -->
           <div class="row q-pa-md q-gutter-md">
-            <div class="col-12 col-md-4 ">
+            <div class="col-12 col-md-4">
+              <q-input class="text-caption full-width" filled label="Brand Name" dense />
+            </div>
+
+            <div class="col-12 col-md-6">
               <q-input class="text-caption full-width" filled label="Generic Name" dense />
             </div>
 
-            <div class="col-12 col-md-6 ">
-              <q-input class="text-caption full-width" filled label="Generic Name" dense />
-            </div>
-
-            <div class="col-12 col-md-2  ">
+            <div class="col-12 col-md-2">
               <q-input class="text-caption full-width" filled label="Expiration Date" dense />
             </div>
 
-            <div class="col-12 col-md-2  ">
+            <div class="col-12 col-md-2">
               <q-input class="text-caption full-width" filled label="Dosage" dense />
             </div>
 
-            <div class="col-12 col-md-2  ">
+            <div class="col-12 col-md-2">
               <q-input class="text-caption full-width" filled label="Current Quantity" dense />
             </div>
           </div>
-           <q-separator />
+          <q-separator />
 
-        <div class="text-subtitle1 text-grey q-pa-md">Quantity to Adjust</div>
+          <div class="text-subtitle1 text-grey q-pa-md">Quantity to Adjust</div>
 
-           <div class="row q-pa-md q-gutter-md">
-            <div class="col-12 col-md-2 ">
+          <div class="row q-pa-md q-gutter-md">
+            <div class="col-12 col-md-2">
+              <q-select
+                class="text-caption full-width"
+                filled
+                label="Adjustment Type"
+                dense
+                :options="AdjustmentTypeSeletiion"
+              />
+            </div>
+            <div class="col-12 col-md-2">
               <q-input class="text-caption full-width" filled label="Adjustment Quantity" dense />
             </div>
           </div>
-              <div class="row q-pa-md q-gutter-md">
-            <div class="col-12 col-md-12 ">
-              <q-input class="text-caption full-width" filled label="Reason for Adjustment" dense />
+          <div class="row q-pa-md q-gutter-md">
+            <div class="col-12 col-md-12">
+              <q-input
+                class="text-caption full-width"
+                filled
+                label="Reason for Adjustment"
+                type="textarea"
+                dense
+              />
             </div>
           </div>
-
         </q-card-section>
 
-       
-        <q-card-actions align="right"> </q-card-actions>
+        <q-card-actions align="right">
+          <div class="q-pa-md q-gutter-md">
+            <q-btn label="Cancel" color="grey" flat />
+            <q-btn label="Submit" color="primary" />
+          </div>
+        </q-card-actions>
       </q-card></div
   ></q-page>
 </template>
-<script></script>
+<script>
+export default {
+  name: 'ItemsAdjustmentForm',
+
+  setup() {
+    return {
+      AdjustmentTypeSeletiion: [
+        'Physical Count Adjustment',
+        'Inventory Reconciliation',
+        'Encoding Error Correction',
+      ],
+    }
+  },
+  data() {
+    return {
+      MedicineData: {},
+      AdjustmentData: {},
+    }
+  },
+}
+</script>
 <style scoped></style>
